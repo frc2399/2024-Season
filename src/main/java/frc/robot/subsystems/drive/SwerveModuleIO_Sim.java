c6b454e5ceb8f086e965340200f73480331048df
Herbert Schilling <hschilling@nasa.gov>
2024-01-30 20:55:02 -0500

    continued working on gyro + encoder implementation in simulation

--- src/main/java/frc/robot/subsystems/drive/SwerveModuleIO_Sim.java
+++ src/main/java/frc/robot/subsystems/drive/SwerveModuleIO_Sim.java
@@ -1,12 +1,16 @@
 package frc.robot.subsystems.drive;
 
+import com.revrobotics.CANSparkBase.ControlType;
+
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.BatterySim;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import frc.robot.Constants;
+import frc.utils.SimEncoder;
 
 public class SwerveModuleIO_Sim implements SwerveModuleIO{
 
@@ -16,6 +20,8 @@ public class SwerveModuleIO_Sim implements SwerveModuleIO{
     private DCMotorSim turnMotor =
       new DCMotorSim(DCMotor.getNeo550(1), Constants.ModuleConstants.kTurningMotorReduction, 0.025);
 
+   private SimEncoder m_turningEncoder = new SimEncoder("turn");
+    private SimEncoder m_drivingEncoder = new SimEncoder("drive");
     // Random initial position to simulate arbitrary starting positions
     private final Rotation2d turnAbsoluteInitialPosition =
       new Rotation2d(Math.random() * 2 * Math.PI); // Random initial position
@@ -25,8 +31,8 @@ public class SwerveModuleIO_Sim implements SwerveModuleIO{
     private double turnVolts = 0.0;
 
     // Sim-Tuned PID Controllers
-    private PIDController turnController = new PIDController(15, 0.0, 0.0);
-    private PIDController driveController = new PIDController(5, 0.0, 0.0);
+    private PIDController m_turningPIDController = new PIDController(15, 0.0, 0.0);
+    private PIDController m_drivingPIDController = new PIDController(5, 0.0, 0.0);
     private SimpleMotorFeedforward driveFeedforward =
         new SimpleMotorFeedforward(0.0, Constants.ModuleConstants.kDrivingFF * 12);
 
@@ -49,14 +55,14 @@ public class SwerveModuleIO_Sim implements SwerveModuleIO{
      };
         
      public double getTurnEncoderPosition(){
-        return 0; 
+      return m_turningEncoder.getDistance();
      };
  
      public void setDesiredDriveSpeedMPS(double speed){
-
+      m_drivingPIDController.setSetpoint(speed);
      };
      public void setDesiredTurnAngle(double angle){
-
+        m_turningPIDController.setSetpoint(angle);
      };
      public double getDriveVolts(){
         return 0;
