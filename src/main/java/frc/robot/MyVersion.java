3720b1706559369bce0c23221ddc1e74e99b4263
Herbert Schilling <hschilling@nasa.gov>
2024-02-23 18:04:55 -0500

    removed from cache

--- src/main/java/frc/robot/MyVersion.java
+++ src/main/java/frc/robot/MyVersion.java
@@ -7,13 +7,13 @@ public final class MyVersion {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2024-Season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 157;
-	public static final String GIT_SHA = "1aad2e56877454103225c58544dd4330820be556";
-	public static final String GIT_DATE = "2024-02-22 18:36";
+	public static final int GIT_REVISION = 159;
+	public static final String GIT_SHA = "bfddf59cd2131e158773f0c3100e8c5a0c8ecad6";
+	public static final String GIT_DATE = "2024-02-22 20:54";
 	public static final String GIT_BRANCH = "auton-crunch-time";
-	public static final String BUILD_DATE = "2024-02-22 19:39";
-	public static final long BUILD_UNIX_TIME = 1708648795869L;
-	public static final int DIRTY = 1;
+	public static final String BUILD_DATE = "2024-02-23 18:03";
+	public static final long BUILD_UNIX_TIME = 1708729384804L;
+	public static final int DIRTY = 0;
 
 	private MyVersion(){}
 }
