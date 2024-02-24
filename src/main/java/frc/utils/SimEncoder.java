0dfc147220beb4a7ac0c685473648fd0f900ef17
Herbert Schilling <hschilling@nasa.gov>
2024-02-24 08:27:13 -0500

    Using alternate to SimDevice

--- src/main/java/frc/utils/SimEncoder.java
+++ src/main/java/frc/utils/SimEncoder.java
@@ -1,31 +1,11 @@
 package frc.utils;
 
-import edu.wpi.first.hal.SimDevice;
-import edu.wpi.first.hal.SimDouble;
-import edu.wpi.first.hal.SimDevice.Direction;
-
 public class SimEncoder {
 
-  private SimDouble distance;
-  private SimDouble speed;
+  private double distance = 0.0;
+  private double speed = 0.0;
 
-  /**
-   * Construct a encoder given a CAN ID.
-   *
-   * @param name Name of the encoder must be a unique name.
-   */
-
-   //we "borrowed" code from Alex 
-   //(https://github.com/Mechanisms-Robotics/HawkingBeta/blob/drivetrain/src/main/java/frc/robot/util/SimEncoder.java)
-   //and then had to update it bc the original createDouble was "deprecated" (outdated)
-   //the only difference was direction instead of the boolean readonly
-   //we chose kOutput bc distance is an output 😻
-   
-   //check if kOutput should be kBidir or kInput 👾
   public SimEncoder(String name) {
-    SimDevice device = SimDevice.create("Encoder[" + name + "]");
-    distance = device.createDouble("Distance", Direction.kOutput, 0);
-    speed = device.createDouble("Speed", Direction.kOutput, 0);
   }
   
   /**
@@ -35,7 +15,7 @@ public class SimEncoder {
    *     calling setSpeed.
    */
   public double getSpeed() {
-    return speed.get();
+    return speed;
   }
 
   /**
@@ -45,7 +25,7 @@ public class SimEncoder {
    *     calling setDistance.
    */
   public double getDistance() {
-    return distance.get();
+    return distance;
   }
 
   /**
@@ -54,7 +34,7 @@ public class SimEncoder {
    * @param speed Speed of the encoder in unit's of the users choice.
    */
   public void setSpeed(double speed) {
-    this.speed.set(speed);
+    this.speed = speed;
   }
 
   /**
@@ -63,6 +43,6 @@ public class SimEncoder {
    * @param distance Distance of the encoder in unit's of the users choice.
    */
   public void setDistance(double distance) {
-    this.distance.set(distance);
+    this.distance = distance;
   }
 }
