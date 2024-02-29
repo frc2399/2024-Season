c05dc86e1de9b2c2339e2f3d2f5ce4efe8f75dbc
John Grezmak <jeg100@case.edu>
2024-02-29 11:12:58 -0500

    Added indicator for aligned to speaker, fix for sim crash

--- src/main/java/frc/robot/MyVersion.java
+++ src/main/java/frc/robot/MyVersion.java
@@ -7,19 +7,13 @@ public final class MyVersion {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2024-Season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 218;
-	public static final String GIT_SHA = "6f355f089a6cc756dc19079a8bb95f9152dc8e45";
-	public static final String GIT_DATE = "2024-02-26 10:58";
-<<<<<<< HEAD
+	public static final int GIT_REVISION = 226;
+	public static final String GIT_SHA = "cd891d4a9b05f3b8e486e2aff2f7e830917c59ca";
+	public static final String GIT_DATE = "2024-02-29 11:08";
 	public static final String GIT_BRANCH = "dev-testing";
-	public static final String BUILD_DATE = "2024-02-29 10:48";
-	public static final long BUILD_UNIX_TIME = 1709221717495L;
-=======
-	public static final String GIT_BRANCH = "final-auton";
-	public static final String BUILD_DATE = "2024-02-26 21:08";
-	public static final long BUILD_UNIX_TIME = 1708999725625L;
->>>>>>> 3414d28b540fddafed7e88b2c21df522df354bd6
-	public static final int DIRTY = 1;
+	public static final String BUILD_DATE = "2024-02-29 11:10";
+	public static final long BUILD_UNIX_TIME = 1709223014799L;
+	public static final int DIRTY = 0;
 
 	private MyVersion(){}
 }
