58611234ef8516d276fc6f9160ab8be4b94cd7cd
Clare Hess <chess25@hb.edu>
2024-03-08 20:42:49 -0500

    fixed bugs

--- src/main/java/frc/robot/MyVersion.java
+++ src/main/java/frc/robot/MyVersion.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class MyVersion {
 	public static final String MAVEN_GROUP = "";
-	public static final String MAVEN_NAME = "2024-Season-4";
+	public static final String MAVEN_NAME = "2024-Season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 237;
-	public static final String GIT_SHA = "21407e5ae33ec84a4e9c03f5f941e2ef54a36ff0";
-	public static final String GIT_DATE = "2024-03-02 09:37";
-	public static final String GIT_BRANCH = "dev-testing";
-	public static final String BUILD_DATE = "2024-03-02 10:08";
-	public static final long BUILD_UNIX_TIME = 1709392083496L;
+	public static final int GIT_REVISION = 241;
+	public static final String GIT_SHA = "01e6731a88359d4eea54a1e4248a0d454d98044c";
+	public static final String GIT_DATE = "2024-03-08 20:27";
+	public static final String GIT_BRANCH = "buckeye-auton";
+	public static final String BUILD_DATE = "2024-03-08 20:41";
+	public static final long BUILD_UNIX_TIME = 1709948470257L;
 	public static final int DIRTY = 1;
 
 	private MyVersion(){}
