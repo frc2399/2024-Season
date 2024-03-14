142831cfec0a5012d8d1918410b2c7a528d9f63b
AnnaL114 <alee24@hb.edu>
2024-03-14 18:05:19 -0400

    changed arm angle

--- src/main/java/frc/robot/MyVersion.java
+++ src/main/java/frc/robot/MyVersion.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class MyVersion {
 	public static final String MAVEN_GROUP = "";
-	public static final String MAVEN_NAME = "2024-Season-4";
+	public static final String MAVEN_NAME = "2024-Season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 237;
-	public static final String GIT_SHA = "21407e5ae33ec84a4e9c03f5f941e2ef54a36ff0";
-	public static final String GIT_DATE = "2024-03-02 09:37";
+	public static final int GIT_REVISION = 241;
+	public static final String GIT_SHA = "35176346800eed820f07a90fa538d292733c7a2d";
+	public static final String GIT_DATE = "2024-03-09 14:48";
 	public static final String GIT_BRANCH = "dev-testing";
-	public static final String BUILD_DATE = "2024-03-02 10:08";
-	public static final long BUILD_UNIX_TIME = 1709392083496L;
+	public static final String BUILD_DATE = "2024-03-13 18:05";
+	public static final long BUILD_UNIX_TIME = 1710371121908L;
 	public static final int DIRTY = 1;
 
 	private MyVersion(){}
