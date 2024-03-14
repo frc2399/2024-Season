c615935498c7f06fb90f637408d5a101cfc53b52
AnnaL114 <alee24@hb.edu>
2024-03-14 18:38:30 -0400

    deleted the nonexistant auton

--- src/main/java/frc/robot/MyVersion.java
+++ src/main/java/frc/robot/MyVersion.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class MyVersion {
 	public static final String MAVEN_GROUP = "";
-	public static final String MAVEN_NAME = "2024-Season-4";
+	public static final String MAVEN_NAME = "2024-Season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 244;
-	public static final String GIT_SHA = "738eb7a6a5bab94ce7b9f8568aeba33813bff16d";
-	public static final String GIT_DATE = "2024-03-10 21:12";
+	public static final int GIT_REVISION = 245;
+	public static final String GIT_SHA = "c5ed4b3e6f0426b14a386305d54d837cc40fce91";
+	public static final String GIT_DATE = "2024-03-12 19:59";
 	public static final String GIT_BRANCH = "buckeye-auton";
-	public static final String BUILD_DATE = "2024-03-12 19:57";
-	public static final long BUILD_UNIX_TIME = 1710291442774L;
+	public static final String BUILD_DATE = "2024-03-14 17:21";
+	public static final long BUILD_UNIX_TIME = 1710454892486L;
 	public static final int DIRTY = 1;
 
 	private MyVersion(){}
