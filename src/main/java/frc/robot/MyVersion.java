5b38b3aba5f8e95139289678674b792d6f1834a0
ShrividyaR <sregadamilli26@hb.edu>
2024-03-14 20:52:52 -0400

    tuned autons

--- src/main/java/frc/robot/MyVersion.java
+++ src/main/java/frc/robot/MyVersion.java
@@ -7,12 +7,12 @@ public final class MyVersion {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2024-Season-1";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 246;
-	public static final String GIT_SHA = "c615935498c7f06fb90f637408d5a101cfc53b52";
-	public static final String GIT_DATE = "2024-03-14 17:38";
+	public static final int GIT_REVISION = 247;
+	public static final String GIT_SHA = "2c6146b732c899da08e43b23f1dc4f67898b64c7";
+	public static final String GIT_DATE = "2024-03-14 19:35";
 	public static final String GIT_BRANCH = "buckeye-auton";
-	public static final String BUILD_DATE = "2024-03-14 19:27";
-	public static final long BUILD_UNIX_TIME = 1710462435139L;
+	public static final String BUILD_DATE = "2024-03-14 19:50";
+	public static final long BUILD_UNIX_TIME = 1710463821628L;
 	public static final int DIRTY = 1;
 
 	private MyVersion(){}
