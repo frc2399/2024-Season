5ae1eb973b419e1b939de2c5fbda14e267d38f03
AnnaL114 <alee24@hb.edu>
2024-03-17 19:38:03 -0400

    error messages

--- src/main/java/frc/robot/MyVersion.java
+++ src/main/java/frc/robot/MyVersion.java
@@ -7,12 +7,12 @@ public final class MyVersion {
 	public static final String MAVEN_GROUP = "";
 	public static final String MAVEN_NAME = "2024-Season";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 266;
-	public static final String GIT_SHA = "4c865286b96c72250a2fa2d2e2b810e8456b8679";
-	public static final String GIT_DATE = "2024-03-17 16:06";
+	public static final int GIT_REVISION = 269;
+	public static final String GIT_SHA = "d6065c4848c35e110d17d78beaee45b67a6aab59";
+	public static final String GIT_DATE = "2024-03-17 17:31";
 	public static final String GIT_BRANCH = "dev-testing";
-	public static final String BUILD_DATE = "2024-03-17 17:05";
-	public static final long BUILD_UNIX_TIME = 1710713106287L;
+	public static final String BUILD_DATE = "2024-03-17 18:17";
+	public static final long BUILD_UNIX_TIME = 1710717468140L;
 	public static final int DIRTY = 1;
 
 	private MyVersion(){}
