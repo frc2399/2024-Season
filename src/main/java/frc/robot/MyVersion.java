fbd145ea7d035c50852f5752beb1dd15473534c6
Maheeshah07 <mshah25@hb.edu>
2024-03-18 18:56:28 -0400

    auto changes 4 and 2 piece

--- src/main/java/frc/robot/MyVersion.java
+++ src/main/java/frc/robot/MyVersion.java
@@ -5,14 +5,14 @@ package frc.robot;
  */
 public final class MyVersion {
 	public static final String MAVEN_GROUP = "";
-	public static final String MAVEN_NAME = "2024-Season";
+	public static final String MAVEN_NAME = "2024-Season-4";
 	public static final String VERSION = "unspecified";
-	public static final int GIT_REVISION = 269;
-	public static final String GIT_SHA = "d6065c4848c35e110d17d78beaee45b67a6aab59";
-	public static final String GIT_DATE = "2024-03-17 17:31";
+	public static final int GIT_REVISION = 272;
+	public static final String GIT_SHA = "132a09b7f7873052950fadba98dd4bfe89a0874f";
+	public static final String GIT_DATE = "2024-03-18 17:31";
 	public static final String GIT_BRANCH = "dev-testing";
-	public static final String BUILD_DATE = "2024-03-17 18:17";
-	public static final long BUILD_UNIX_TIME = 1710717468140L;
+	public static final String BUILD_DATE = "2024-03-18 17:55";
+	public static final long BUILD_UNIX_TIME = 1710802559830L;
 	public static final int DIRTY = 1;
 
 	private MyVersion(){}
