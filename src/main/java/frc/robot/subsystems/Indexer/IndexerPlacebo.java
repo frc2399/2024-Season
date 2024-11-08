d8b3c2ccb1f2006a6df41a74e6cef1150bc17030
Aashvi Jagetia <ajagetia26@hb.edu>
2024-11-07 20:52:20 -0500

    added placebo files

---
+++ src/main/java/frc/robot/subsystems/Indexer/IndexerPlacebo.java
@@ -0,0 +1,39 @@
+package frc.robot.subsystems.Indexer;
+
+public class IndexerPlacebo implements IndexerIO {
+    public void setMotor(double indexerVelocity) {
+        
+    }
+
+    public double getCurrent() {
+        return 0;
+    }
+
+    public double getVelocity() {
+        return 0;
+    }
+
+    public double getEncoderPosition() {
+        return 0;
+    }
+
+    public void setCurrentLimit(int current) {
+     
+    }
+
+    public void periodicUpdate() {
+
+    }
+
+    public boolean getIsBeamBroken() {
+        return false;
+    }
+
+    public void setIsOverride() {
+
+    }
+
+    public void setIndexerPID(double indexerVelocity) {
+
+    }
+}
---
+++ src/main/java/frc/robot/subsystems/intake/IntakePlacebo.java
@@ -0,0 +1,39 @@
+package frc.robot.subsystems.intake;
+
+public class IntakePlacebo implements IntakeIO {
+    public void setMotor(double intakeVelocity) {
+
+    }
+
+    public double getLeftVelocity() {
+        return 0;
+    }
+
+    public double getRightVelocity() {
+        return 0;
+    }
+
+    public double getLeftEncoderPosition() {
+        return 0;
+    }
+
+    public double getRightEncoderPosition() {
+        return 0;
+    }
+
+    public void setLeftCurrentLimit(int current) {
+
+    }
+
+    public void setRightCurrentLimit(int current) {
+
+    }
+
+    public void periodicUpdate() {
+
+    }
+
+    public void setIntakePID(double percentOutput) {
+
+    }
+}
